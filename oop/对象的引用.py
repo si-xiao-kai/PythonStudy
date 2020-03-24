5dfff2d6c5a8dd9216b589b0bbacb5045f39d823
si-xiao-kai <kai388397@163.com>
2020-03-24 22:52:04 +0800

    对象的引用

---
+++ oop/对象的引用.py
@@ -0,0 +1,17 @@
+class Cat:
+    def eat():
+        print("小猫吃鱼")
+    def drink():
+        print("小猫喝水")
+def main():
+    tom=Cat()
+    print("print函数直接输出接受对象的变量：")
+    print(tom)
+    addr=id(tom)
+    print("通过id函数获得十进制形式的引用地址：%d" % addr)
+    print("通过id函数获得十六进制形式的引用地址：%x" % addr)
+
+
+
+if __name__ == "__main__":
+    main()
